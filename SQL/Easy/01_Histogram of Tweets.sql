9d7da41252b3245c4b738fe3edbbab2f489c4955
Keshav Thakur <keshavthakur.001@gmail.com>
2024-08-06 00:07:24 +0530

    Solve 01_Histogram of Tweets.

---
+++ SQL/Easy/01_Histogram of Tweets.sql
@@ -0,0 +1,11 @@
+WITH tweet_count AS
+(
+  SELECT user_id, COUNT(*) as tweet_bucket 
+  FROM tweets
+  WHERE EXTRACT(YEAR FROM tweet_date) = 2022
+  GROUP BY user_id
+)
+
+SELECT tweet_bucket, COUNT(*) AS users_num 
+FROM tweet_count
+GROUP BY tweet_bucket ORDER BY 1
